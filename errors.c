36b6dabeea5518d1091e03876c2b0eaa02f6214d
Akande Lateef <Akandelateef0@gmail.com>
2023-04-26 22:04:11 -0700

    error controlled

---
+++ errors.c
@@ -0,0 +1,62 @@
+#include "shell.h"
+/**
+* _eputs - return input string
+* @str: string input
+* Return: Nothing
+*/
+void _eputs(char *str)
+{
+	int i = 0;
+
+	if (!str)
+		return;
+	while (str != '\0')
+	{
+		_eputchar(str[i]);
+		i++;
+	}
+}
+
+/**
+* _eputchar - writes to std error
+* @c: character to print
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _eputchar(char c)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(2, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
+/**
+* _putfd - supply chars to fd
+* @c: The char supplied
+* @fd: file descriptor
+* Return: On success 1.
+* On error, -1 is returned, and errno is set appropriately.
+*/
+int _putfd(char c, int fd)
+{
+	static int i;
+	static char buf[WRITE_BUF_SIZE];
+
+	if (c == BUF_FLUSH || i >= WRITE_BUF_SIZE)
+	{
+		write(fd, buf, i);
+		i = 0;
+	}
+	if (c != BUF_FLUSH)
+		buf[i++] = c;
+	return (1);
+}
+
